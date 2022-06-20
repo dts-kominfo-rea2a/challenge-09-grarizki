9d9ce5c624b6b8eb421b9a76e8c677afceba741d
grarizki <28901232+grarizki@users.noreply.github.com>
2022-06-20 14:59:40 +0700

    tugas intro react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './contact.css';
+import '../data/contacts.json'
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <>
+            <div className="card">
+                <div className="container">
+                    <img className='img' src={data.photo} alt={data.name} />
+                    <div className="content">
+                        <h2>{data.name}</h2>
+                        <p>{data.phone}</p>
+                        <p>{data.email}</p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './header.css';
 
 const Header = () => {
+
     return (
-        <></>
+
+        <div className="title">
+            <h1>Call a Friend </h1>
+            <p> your friendly contact app</p>
+        </div >
     )
 }
 
